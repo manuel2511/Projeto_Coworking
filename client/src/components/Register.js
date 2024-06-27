8125598f768f26b3b262e78d0ec30b407eda8580
Tarsila <tarsilabfritz0305@outlook.com>
2024-06-27 20:36:16 -0300

    Login e Registro

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -5,6 +5,7 @@ import './register.css';
 const Register = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
   const [email, setEmail] = useState('');
 
   const handleSubmit = async (e) => {
@@ -18,22 +19,58 @@ const Register = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Register</h2>
-      <div>
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-        <label>Username</label>
-      </div>
-      <div>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <label>Password</label>
-      </div>
-      <div>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <label>Email</label>
-      </div>
-      <button type="submit">Register</button>
-    </form>
+    <div className='register-container'>
+      <form onSubmit={handleSubmit}>
+        <h1>COWORKING SPACE</h1>
+        <div className='register-content'>
+          <h2>Register</h2>
+          <div className='register-form-group'>
+            <input 
+              type="email"
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder=''
+            />
+            <label>Email</label>
+          </div>
+          <div className='register-form-group'>
+            <input 
+              type="text" 
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)} 
+              required
+              placeholder=''
+            />
+            <label>Username</label>
+          </div>
+          <div className='register-form-group'>
+            <input 
+              type="password" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              required
+              placeholder=''
+            />
+            <label>Senha</label>
+          </div>
+          <div className='register-form-group'>
+            <input 
+              type="password"
+              value={repeatPassword} 
+              onChange={(e) => setRepeatPassword(e.target.value)} 
+              required
+              placeholder=''
+            />
+            <label>Repetir Senha</label>
+          </div>
+          <div className='register-footer'>
+            <button className='register-btn' type="submit">Register</button>
+            <a href='/login' className='register-login-btn'>Já possui cadastro? Faça login</a>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
 
