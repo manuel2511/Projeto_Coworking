37324ffb1708f58092a57da0b66f69a443dd39d9
manuel2511 <tekmanuel@gmail.com>
2024-06-26 21:01:28 -0300

    Atualização da Tela principal

--- client/src/components/Index.js
+++ client/src/components/Index.js
@@ -7,7 +7,17 @@ function Index() {
     <>
       <Header />
       <NavBar />
-      <h1>Index</h1>
+      <div class="pagetitle">
+      <h1>Form Layouts</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="/">Home</a></li>
+          <li class="breadcrumb-item">Forms</li>
+          <li class="breadcrumb-item active">Layouts</li>
+        </ol>
+      </nav>
+    </div>
+    {/* <!-- End Page Title --> */}
       <Footer />
     </>
   );
--- client/src/components/Padroes/TelaPadrao.js
+++ client/src/components/Padroes/TelaPadrao.js
@@ -2,7 +2,16 @@ import React from "react";
 function FormPadrao() {
   return (
     <div>
-      <h1>Tela Padrão</h1>
+      <div class="pagetitle">
+      <h1>Form Layouts</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="/">Home</a></li>
+          <li class="breadcrumb-item">Forms</li>
+          <li class="breadcrumb-item active">Layouts</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
     </div>
   );
 }
