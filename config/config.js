a7bd269a5147c383a1cb7a7b6fe886249c017443
manuel2511 <tekmanuel@gmail.com>
2024-07-09 12:48:39 -0300

    Ajuste config

--- config/config.js
+++ config/config.js
@@ -23,7 +23,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_PROD,
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
+    port: parseInt(process.env.DB_PORT) || 3306,
     dialect: 'mysql'
   }
 };
